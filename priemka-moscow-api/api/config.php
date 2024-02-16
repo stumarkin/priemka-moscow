555a1b91da4b0175b5f7ad7ff8d1ef9d19940986
stumarkin <stumarkin@mail.ru>
2024-02-16 18:26:53 +0300

    config template

---
+++ priemka-moscow-api/api/config.php
@@ -0,0 +1,20 @@
+<?
+/**
+ * @author Sergey Tumarkin https://tumarkin.me
+ */
+
+const DB_SERVERNAME = "localhost";
+const DB_USERNAME = "dbuiser";
+const DB_PASSWORD = "dbpass";
+const DB_DBNAME = "dbname";
+
+const AMPLITUDE_KEY = "12345678901234567890123456789012";
+const AMPLITUDE_PROJECT = "12345678901234567890123456789012";
+
+const DOMAIN= "https://example.com/";
+
+const TOKEN_KEY = 'qwerty';
+
+const BOT_TOKEN = "0000000000:123456789123456789123456789123456789123456789";
+const LOG_CHANNEL = "-100";
+?>
