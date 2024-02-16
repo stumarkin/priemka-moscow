66851fb81bb6824f713dc51fc6441cb9ea7d1878
stumarkin <stumarkin@mail.ru>
2024-02-16 16:53:49 +0300

    added config template

---
+++ priemka-moscow-app/data/Config.jsx
@@ -0,0 +1,8 @@
+/**
+ * @author Sergey Tumarkin https://tumarkin.me
+ */
+
+export const CompanyName = "Приёмка.Москва";
+export const Domain = "https://example.com/";
+export const ApiPath = "api/";
+export const AmplitudeKey = '12345678901234567890123456789012';
