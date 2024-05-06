b2d7c1856c27d253f5c38ab36e22c3519f5ced0a
stumarkin <stumarkin@mail.ru>
2024-05-06 13:59:11 +0300

    v2.2.3

--- priemka-moscow-api/api/config.php
+++ priemka-moscow-api/api/config.php
@@ -4,17 +4,37 @@
  */
 
 const DB_SERVERNAME = "localhost";
-const DB_USERNAME = "dbuiser";
-const DB_PASSWORD = "dbpass";
-const DB_DBNAME = "dbname";
+const DB_USERNAME = "";
+const DB_PASSWORD = ""; 
+const DB_DBNAME = "";
 
-const AMPLITUDE_KEY = "12345678901234567890123456789012";
-const AMPLITUDE_PROJECT = "12345678901234567890123456789012";
+const AUTH_DAYS = 7;
 
-const DOMAIN= "https://example.com/";
+const AMPLITUDE_KEY = "";
+const AMPLITUDE_PROJECT = "";
 
-const TOKEN_KEY = 'qwerty';
+const TARGET_FILECV_DIR = "/upload/";
+const TARGET_USERFILE_DIR = "/upload/users/";
+const TARGET_ACCOUNTFILE_DIR = "/upload/accounts/";
 
-const BOT_TOKEN = "0000000000:123456789123456789123456789123456789123456789";
+const S3_ENDPOINT = "";
+const S3_KEY = "";
+const S3_SECRET = "";
+const S3_BUCKET = "";
+
+$bucketName = '';
+
+
+const DOMAIN= "";
+
+const TOKEN_KEY = '';
+
+const BOT_TOKEN = "";
 const LOG_CHANNEL = "-100";
+
+$featuretoggles = [
+    'offline' => true,
+    'photo' => true,
+];
+
 ?>
